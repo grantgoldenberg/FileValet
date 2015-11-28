fbcb4e456a38964f3345997842a3ae7ff701b3d2
grantgoldenberg <grant.goldenberg@gmail.com>
2015-11-28 13:46:41 -0500

    Fixed File Location
    grant.goldenberg@gmail.com

--- Cleaner.java
+++ Cleaner.java
@@ -1,20 +1,18 @@
+
 import java.util.Scanner;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FilenameFilter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 /**
- * Write a description of class Cleaner here.
+ * Program made to clean your files (MAC version).
  * 
  * @author (Grant Goldenberg) 
  * @version (November 27, 2015)
  */
-public class Cleaner
+public class Cleaner 
 {
-    public static char init(){
+    public static char init() {
         System.out.println("\fWelcome to File Valet the program made to clean your files!");
         System.out.print("Would you like us to clean your desktop (Y/n): ");
         Scanner keyboard = new Scanner(System.in);
@@ -22,34 +20,31 @@ public class Cleaner
         return input;
     }
 
-    public static void cleaner(char input) {
+    public static void cleaner(char input) throws IOException {
         if (input == 'Y') {
-            System.out.println("Cleaning...");
-            InputStream inStream = null;
-            OutputStream outStream = null;
-            File desktop = new File("C:/Users/grant/Desktop");
-            File[] listOfFiles = desktop.listFiles();
-            for(int i = 0; i < listOfFiles.length; i++) {
-                String filename = listOfFiles[i].getName();
-                if(filename.startsWith("#z")){
-                    try {
-                        File bfile = new File("C:/Users/grant/Documents");
-                        inStream = new FileInputStream(desktop);
-                        outStream = new FileOutputStream(bfile);
-                        byte[] buffer = new byte[1024];
-                        int length;
-                        while ((length = inStream.read(buffer)) > 0){
-                            outStream.write(buffer, 0, length);
+            System.out.println("Okay, cleaning...");
+            String dirPath = "/Users/grantgoldenberg/Desktop";
+            File dir = new File(dirPath);
+            FilenameFilter textFilter = new FilenameFilter() {
+                    public boolean accept(File dir, String name) {
+                        String lowercaseName = name.toLowerCase();
+                        if (lowercaseName.startsWith("#z")) {
+                            System.out.println("The following files were found and are being cleaned.");
+                            return true;
+                        } else {
+                            System.out.println("No files were found to be cleaned.");
+                            return false;
                         }
-                        inStream.close();
-                        outStream.close();
-                        desktop.delete();
-                        System.out.println("File has been sorted successfully!");
                     }
-                    catch(IOException e){
-                        e.printStackTrace();
-                    }
+                };
+            File[] files = dir.listFiles(textFilter);
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    System.out.print("directory:");
+                } else {
+                    System.out.print("file:");
                 }
+                System.out.println(file.getCanonicalPath());
             }
         }
         else {
@@ -58,7 +53,7 @@ public class Cleaner
         }
     }
 
-    public static void main(String args[]) {    
+    public static void main(String args[]) throws IOException {    
         cleaner(init());
     }
 }
